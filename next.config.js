5af5820efd619d66054d9c30ececcaf08ac6ce21
baidefeis <alk2yk@gmail.com>
2025-08-22 12:59:52 +0200

    commit2

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
+  webpack: (config, { isServer }) => {
+    // Fix for module resolution in Vercel
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
   },
+  // Output file tracing for better Vercel compatibility
+  outputFileTracingRoot: __dirname,
 }
 
 module.exports = nextConfig
